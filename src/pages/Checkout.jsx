4e3680c666e13c03e017e49927f665824addc7d4
TwentySeV <112863484+vanniichan@users.noreply.github.com>
2025-02-21 00:21:51 +0700

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -43,15 +43,15 @@ const Checkout = () => {
                 <div className="card-body">
                   <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                      Products ({totalItems})<span>{Math.round(subtotal)}.000 VND</span>
+                      Sản phẩm ({totalItems})<span>{Math.round(subtotal)}.000 VND</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between align-items-center px-0">
-                      Shipping
+                      Phí ship
                       <span>{shipping}.000 VND</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                       <div>
-                        <strong>Total amount</strong>
+                        <strong>Tổng tiền</strong>
                       </div>
                       <span>
                         <strong>{Math.round(subtotal + shipping)}.000 VND</strong>
